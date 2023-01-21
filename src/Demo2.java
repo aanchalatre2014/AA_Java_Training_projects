cde27b5a6fbb9b2df1575065071ca1a5dfe8dacd
aanchalatre2014 <aanchalatre2014@gmail.com>
2023-01-21 20:37:06 +0530

    Demo Project Training

---
+++ src/Demo2.java
@@ -0,0 +1,7 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("It is a wonderful day");
+        System.out.println("Weather is cool");
+        System.out.println("winter season ");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello My name is Aanchal Atre");
+        System.out.println("I stay in mumbai");
+        System.out.println("Today is thursday");
+    }
+}
